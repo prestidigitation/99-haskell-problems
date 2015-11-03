6516d202c19282123fa6a5ff9c889e453fe02f79
prestidigitation <alexander.c.rowland@gmail.com>
2015-11-03 16:47:24 -0500

    More list problems completed

--- 99_problems/1-10_lists.hs
+++ 99_problems/1-10_lists.hs
@@ -1,6 +1,5 @@
 -- Problem 1
 -- Find the last element of a list.
-
 myLast :: [a] -> a
 myLast [] = error "empty list"
 myLast [x] = x
@@ -9,7 +8,6 @@ myLast (_:xs) = myLast xs
 
 -- Problem 2
 -- Find the last but one element of a list.
-
 myButLast :: [a] -> a
 myButLast [] = error "empty list"
 myButLast (x:xs)
@@ -23,7 +21,6 @@ myButLast' = head . tail . reverse
 
 -- Problem 3
 -- Find the K'th element of a list. The first element in the list is number 1.
-
 elementAt :: [a] -> Int -> a
 elementAt [] _ = error "empty list"
 elementAt xs n = head (drop (n - 1) xs)
@@ -37,5 +34,27 @@ elementAt' xs n = xs !! (n - 1)
 myLength :: [a] -> Int
 myLength []     = 0
 myLength (x:xs) = 1 + myLength xs
+
+-- Interesting alternative solution using map.
+-- Changes all values in the list to 1 and then totals them.
+myLength' :: [a] -> Int
+myLength' xs = sum (map (\x -> 1) xs)
+
+
+-- Problem 5
+-- Reverse a list.
+myReverse :: [a] -> [a]
+myReverse []     = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+myReverse' :: [a] -> [a]
+myReverse' [] = []
+myReverse' xs = [last xs] ++ myReverse' (init xs)
+
+
+-- Problem 6
+-- Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = reverse xs == xs
 
 
