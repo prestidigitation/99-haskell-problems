2a4e7e934ef580c675ee568592f5785aebb50918
Alexander Rowland <rowlanda@protonmail.com>
2021-07-28 22:42:59 -0700

    Question 2

--- 1-10.hs
+++ 1-10.hs
@@ -1,5 +1,13 @@
--- Find the last element of a list
+-- 1. Find the last element of a list.
 myLast :: [a] -> a
-myLast [] = error "List is empty!"
+myLast [] = error "No last element for empty list!"
 myLast [x] = x
-myLast (x:xs) = myLast xs
+myLast (_:xs) = myLast xs
+
+-- 2. Find the last but one element of a list.
+myButLast :: [a] -> a
+myButLast [] = error "No second to last element for empty list!"
+myButLast [x] = error "No second to last element for singleton list!"
+myButLast (x:xs)
+  | length xs == 1 = x
+  | otherwise = myButLast xs
